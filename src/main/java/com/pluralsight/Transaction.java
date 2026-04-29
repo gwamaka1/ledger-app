1d3879f0b990a32936ab4c7dbfc04b73fedc4f18
gwamaka1 <mwamtobegwamaka@gmail.com>
2026-04-29 11:38:42 -0400

    created the transaction object

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
 public class Transaction {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double transaction;
+
+    public Transaction(String date, String time, String description, String vendor, double transaction) {
+        this.time = time;
+        this.date = date;
+        this.description = description;
+        this.vendor = vendor;
+        this.transaction = transaction;
+    }
+
+    public double getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(double transaction) {
+        this.transaction = transaction;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String companyName) {
+        this.vendor = vendor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
